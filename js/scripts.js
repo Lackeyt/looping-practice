164807a93522040e430a8aca02379dcd96c75e5f
Erich Richter <erich.richter@gmail.com>
2020-06-09 13:19:27 -0700

    add js functionality > > co-authored-by: Allison Sadin <aesadin@gmail.com> Tyson Lackey <lackeyt90@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,51 @@
 $(document).ready(function(){
+  $("#wordPuzzle").submit(function(event){
+    event.preventDefault()
 
+    const input = $("input#q1").val();
+    let splitString = input.split("");
+
+//Checks if inputted character is a vowel, returns Boolean
+    function isVowel( chr ){ 
+      return 'aeiou'.indexOf( chr[0].toLowerCase() ) !== -1;
+    };
+
+//Replaces Vowels with "-" in a string, returns String
+    function vowelReplace(splitString){
+      for(i=0;i <splitString.length; i++){
+        if (isVowel(splitString[i])){
+          splitString[i] = "-";
+        };
+      };
+      return splitString.join("");
+    };
+
+//Output
+    $("p.outputTxt").text(vowelReplace(splitString));
+    $("form#wordPuzzle").hide();
+    $("div.output").show();
+  });
+
+//Button click Refreshes page
+  $(".startOver").click(function() {
+    location.reload();
+  })
+
+  //Checks if the submitted answers was correct
+  $("#answerVal").submit(function(event){
+    event.preventDefault()
+    const input = $("input#q1").val().toLowerCase();
+    const answer = $("input#q2").val().toLowerCase();
+
+    if (answer === input) {
+      $("form#wordPuzzle").hide();
+      $("div.output").hide();
+      $("div.answerSuccess").show();
+    } else {
+      $("form#wordPuzzle").hide();
+      $("div.output").hide();
+      $("div.answerFail").show();
+      $(".correctAnswer").text(input)
+    }
+  });
 });
-
-for (i=0; i<50; i+=7){
-  
-  console.log(i)
-}
